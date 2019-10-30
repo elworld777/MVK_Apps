e0d154de1897278d548495c6fbf8ca8329fb3372
elworld777 <elworld777@gmail.com>
2019-10-30 10:21:11 +0500

    changes

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 command = '/usr/html/MVK_Apps/env/bin/gunicorn'
 pythonpath = '/usr/html/MVK_Apps/MVK_Apps'
 bind = '127.0.0.1:8001'
-workers =9
+workers = 9
 user = 'ipcamer'
 limit_request_fieds = 32000
 limit_request_field_size = 0
