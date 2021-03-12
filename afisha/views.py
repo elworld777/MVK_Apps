1fa4ba966218d5a7bd01b5bf3809c8fbbb5ec557
elworld777 <56360085+elworld777@users.noreply.github.com>
2021-03-12 15:59:25 +0500

    1

--- afisha/views.py
+++ afisha/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from afisha.models import Category
 
 # Create your views here.
+
+class ListMaps(ListView):
+    model = Category
+    # queryset = Maps.objects.all().order_by('id')
+    context_object_name = 'category'
+    # template_name='RomanMaps/Maps.html'
+
+def index(request):
+    category = Category.objects.all()
+    context = {
+        'category_list' : category
+    }
+    return render(request, 'afisha/category_list.html', context)
