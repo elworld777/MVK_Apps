4914beb8ebd1180111cc31be3cc6fe5731b3867d
elworld777 <56360085+elworld777@users.noreply.github.com>
2021-03-12 17:39:08 +0500

    Update views.py

--- afisha/views.py
+++ afisha/views.py
@@ -4,15 +4,25 @@ from afisha.models import Category
 
 # Create your views here.
 
+
 class ListMaps(ListView):
     model = Category
     # queryset = Maps.objects.all().order_by('id')
     context_object_name = 'category'
     # template_name='RomanMaps/Maps.html'
 
+
 def index(request):
     category = Category.objects.all()
     context = {
-        'category_list' : category
+        'category_list': category
+    }
+    return render(request, 'category_list.html', context)
+
+
+def exhibitions(request):
+    category = Category.objects.all()
+    context = {
+        'category_list': category
     }
     return render(request, 'category_list.html', context)
