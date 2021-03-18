9d07040fb3b0f1bfeaadf27bc61f2ee7b82f6b42
elworld777 <56360085+elworld777@users.noreply.github.com>
2021-03-18 11:01:35 +0500

    Update urls.py

--- afisha/urls.py
+++ afisha/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ar', views.ar, name='ar'),
+    path('ar/', views.ar, name='ar'),
     path('<str:pk>', views.exhibitions, name='exhibitions'),
     path('<str:pk>/<int:id>', views.entry, name='entry'),
 ]
