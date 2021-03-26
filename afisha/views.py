25d78ba8c7652990fb7f2528bded2b82a2b88e7d
elworld777 <56360085+elworld777@users.noreply.github.com>
2021-03-26 17:04:14 +0500

    1

--- afisha/views.py
+++ afisha/views.py
@@ -14,7 +14,8 @@ class ListMaps(ListView):
 
 
 def index(request):
-    category = Category.objects.filter(parent_category=None).order_by('priority')
+    category = Category.objects.filter(
+        parent_category=None).order_by('priority')
     setting = Setting.objects.all().first()
     context = {
         'category_list': category,
@@ -25,7 +26,8 @@ def index(request):
 
 def exhibitions(request, pk):
     try:
-        category = Category.objects.filter(url=pk).first().category_set.all().order_by('priority')
+        category = Category.objects.filter(
+            url=pk).first().category_set.all().order_by('priority')
         entry = Category.objects.filter(url=pk).first().entry_set.all()
     except:
         category = None
@@ -61,12 +63,15 @@ def ar(request):
     }
     return render(request, 'ar_list.html', context)
 
+
 def info(request):
     return render(request, 'info.html')
 
+
 def muzart(request):
     try:
-        category = Category.objects.filter(url="muzart").first().category_set.all().order_by('priority')
+        category = Category.objects.filter(
+            url="muzart").first().category_set.all().order_by('priority')
         entry = Category.objects.filter(url="muzart").first().entry_set.all()
     except:
         category = None
@@ -78,9 +83,11 @@ def muzart(request):
     }
     return render(request, 'muzart_category.html', context)
 
+
 def muzart_list(request, id):
     try:
-        category = Category.objects.filter(url="muzart").first().category_set.all().order_by('priority')
+        category = Category.objects.filter(
+            url="muzart").first().category_set.all().order_by('priority')
         entry = Category.objects.filter(url="muzart").first().entry_set.all()
     except:
         category = None
@@ -92,3 +99,14 @@ def muzart_list(request, id):
         'id': id
     }
     return render(request, 'muzart_list.html', context)
+
+
+def eco(request):
+    try:
+        entry = Category.objects.filter(url="eco").first().entry_set.all()
+    except:
+        entry = None
+    context = {
+        'entry_list': entry
+    }
+    return render(request, 'slider.html', context)
