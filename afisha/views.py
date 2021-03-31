bb2dcbdab7360e819818aa3ee33570af8d85e48a
elworld777 <56360085+elworld777@users.noreply.github.com>
2021-03-31 11:53:04 +0500

    Update views.py

--- afisha/views.py
+++ afisha/views.py
@@ -104,7 +104,7 @@ def muzart_list(request, id):
 
 def eco(request):
     try:
-        entry = Category.objects.filter(url="eco").first().entry_set(is_active=True)
+        entry = Category.objects.filter(url="eco").first().entry_set(is_active=True).all()
     except:
         entry = None
     context = {
