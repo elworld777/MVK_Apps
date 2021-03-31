fd0d46c57341df7b4dca1b2e16f095ef34e69a38
elworld777 <56360085+elworld777@users.noreply.github.com>
2021-03-31 15:30:31 +0500

    Update models.py

--- afisha/models.py
+++ afisha/models.py
@@ -3,9 +3,9 @@ from datetime import date
 from django.utils.functional import cached_property
 
 
-class EntryManager(models.Manager):
+""" class EntryManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(Q(active=True), Q(date_end__gte=date.today() | Q(date_end=None))
+        return super().get_queryset().filter(Q(active=True), Q(date_end__gte=date.today() | Q(date_end=None)) """
 
 
 class Category(models.Model):
