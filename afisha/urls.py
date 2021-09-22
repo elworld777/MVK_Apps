8a51816318bcbefc59cf783b9c9afd9856948e91
elworld777 <56360085+elworld777@users.noreply.github.com>
2021-09-22 10:13:46 +0500

    Update urls.py

--- afisha/urls.py
+++ afisha/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('muzart', views.muzart, name='muzart'),
     path('excurs', views.excurs, name='excurs'),
     path('slider', views.slider, name='slider'),
+    path('slider1', views.slider, name='slider'),
     path('exhibit', views.exhibit, name='exhibit'),
     path('test', views.test, name='test'),
     path('muzart/<int:id>', views.muzart_list, name='muzart_list'),
