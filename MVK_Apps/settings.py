fda9e0beff8e9a9f6fae7ad153f16ec07bc3089b
elworld777 <56360085+elworld777@users.noreply.github.com>
2024-04-03 10:54:02 +0500

    update

--- MVK_Apps/settings.py
+++ MVK_Apps/settings.py
@@ -141,3 +141,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
